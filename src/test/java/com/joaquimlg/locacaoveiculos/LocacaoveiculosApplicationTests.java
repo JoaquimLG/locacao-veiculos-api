c58371e8a4a3589a24f6e621a5a78f7e79b2a8c0
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-04-14 10:28:27 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/com/joaquimlg/locacaoveiculos/LocacaoveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquimlg.locacaoveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
