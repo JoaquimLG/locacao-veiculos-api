354c8dfc32e1ed21fc9dc0cb90c1c45e9d50c345
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-04-27 20:23:45 -0300

    feat: implementar listagem de clientes

--- src/main/java/com/joaquimlg/locacaoveiculos/controller/ClienteController.java
+++ src/main/java/com/joaquimlg/locacaoveiculos/controller/ClienteController.java
@@ -1,4 +1,25 @@
 package com.joaquimlg.locacaoveiculos.controller;
 
+import com.joaquimlg.locacaoveiculos.entity.Cliente;
+import com.joaquimlg.locacaoveiculos.service.ClienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public List<Cliente> listarClientes() {
+        return clienteService.listarClientes();
+    }
+
 }
--- src/main/java/com/joaquimlg/locacaoveiculos/service/ClienteService.java
+++ src/main/java/com/joaquimlg/locacaoveiculos/service/ClienteService.java
@@ -1,4 +1,20 @@
 package com.joaquimlg.locacaoveiculos.service;
 
+import com.joaquimlg.locacaoveiculos.entity.Cliente;
+import com.joaquimlg.locacaoveiculos.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
 }
