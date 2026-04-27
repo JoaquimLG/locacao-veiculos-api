371b6c06fa2f864f740675e0ccfa5661d2565d71
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-04-27 20:13:42 -0300

    feat: adicionar estrutura da entidade cliente

--- src/main/java/com/joaquimlg/locacaoveiculos/entity/Cliente.java
+++ src/main/java/com/joaquimlg/locacaoveiculos/entity/Cliente.java
@@ -1,4 +1,21 @@
 package com.joaquimlg.locacaoveiculos.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table (name = "clientes")
+
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
 }
